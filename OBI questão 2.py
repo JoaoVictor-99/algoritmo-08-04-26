4fb634f24a9d4f1ef681b86b52881c5f5a695692
JoaoVictor-99 <joaosuemia@gmail.com>
2026-04-08 08:20:50 -0300

    Add files via upload

---
+++ OBI questão 2.py
@@ -0,0 +1,14 @@
+n = int(input())
+r = int(input())
+P = int(input())
+
+total = n
+novos = n
+
+for dia in range(1000000):
+    if total >= p:
+        print(dia)
+        break
+
+        novos *= r
+        total += novos
